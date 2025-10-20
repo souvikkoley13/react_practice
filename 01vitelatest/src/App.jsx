7b12d225e085414126342bd9c9707e540fd29d20
souvikkoley13 <souvik1309@gmail.com>
2025-10-20 13:01:59 +0530

    part 2

--- 01vitelatest/src/App.jsx
+++ 01vitelatest/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
-
+import Test from "./Test"
 import viteLogo from '/vite.svg'
 
 
 function App() {
 
   return (
+    <>
     <h1>souvik is react</h1>
+    <Test/>
+    </>
+    
   )
 }
 
---
+++ 01vitelatest/src/Test.jsx
@@ -0,0 +1,7 @@
+function Test(){
+    return(
+        <h3>chai is ready</h3>
+    )
+}
+
+export default Test
